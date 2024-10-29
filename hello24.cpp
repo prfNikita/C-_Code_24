4c62907c3260843298f882903faaa88cb0a10e10
prfNikita <117563269+prfNikita@users.noreply.github.com>
2024-10-29 07:58:14 +0530

    Create hello24.cpp

---
+++ hello24.cpp
@@ -0,0 +1,12 @@
+// Simple C++ program to display "Hello C++"
+// Header file for input output functions
+#include<iostream>
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{    
+    cout<<"Hello C++";/* prints hello C++*/
+    return 0;
+}
